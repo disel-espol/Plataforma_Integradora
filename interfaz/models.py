5c501d974635d214fbb7cce2a1669a23e4b2e311
Sofia Rodriguez <sbrodrig@espol.edu.ec>
2020-12-02 22:21:01 -0500

    empezando con views y urls

--- interfaz/models.py
+++ interfaz/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class HardwareType(models.Model):
-	name = models.Charfield(max_length=10)
+	name = models.CharField(max_length=10)
 	specifications = models.TextField(max_length=1000)
 	def __str__(self):
 		return self.name
 
 
 class OSType(models.Model):
-	name = models.Charfield(max_length=20)
+	name = models.CharField(max_length=20)
 	def __str__(self):
 		return self.name
--- interfaz/views.py
+++ interfaz/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import HardwareType, OSType
 
 # Create your views here.
+
+def index(request):
+	#hw_list = HardwareType.objects.all()
+
+def run_test(request):
+	return HttpResponse('Aqui van los graficos')
