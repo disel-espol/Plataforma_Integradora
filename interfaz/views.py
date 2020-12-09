ea54edf4dbac0c3fa6117c00ddae8234301fd4c6
sbrodrig <sbrodrig@espol.edu.ec>
2020-12-09 16:17:11 -0500

    cambios menores

--- interfaz/views.py
+++ interfaz/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext
 from .models import Rbdms, HardwareType, OSType, Test, DbConfig
 from .forms import TestForm
+import subprocess
 
 # Create your views here.
 
@@ -11,14 +12,17 @@ def index(request):
 		form = TestForm(request.POST)
 		if(form.is_valid()):
 			print("form is valid")
+			temp = form.cleaned_data.get('hw_type')
+			print(temp)
 			"""
 			TODO:
 				Ejecutar bash script
 			"""
+			#subprocess.call('/home/user/test.sh')
 			return redirect('results')
 	else:
 		print("carga")
-		form = TestForm(request.POST)
+		form = TestForm()
 	return render(request, 'index.html', {'form':form})
 
 def results(request):
