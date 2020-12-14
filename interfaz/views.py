5cc9081537c00949a4e659be5373d09d8c8c036e
sbrodrig <sbrodrig@espol.edu.ec>
2020-12-13 22:49:14 -0500

    bases seleccionadas en variables separadas

--- interfaz/views.py
+++ interfaz/views.py
@@ -13,19 +13,16 @@ def index(request):
 		if(form.is_valid()):
 			print("form is valid")
 			rdbmss = form.cleaned_data.get('rdbms')
-			rdbmsL = list(rdbmss)
+			db1, *dbs = list(rdbmss)
+			db2, db3 = '', ''
+			if(len(dbs) >= 1):
+				db2 = dbs[0]
+			if(len(dbs) == 2):
+				db3 = dbs[1]
 			hwT = form.cleaned_data.get('hw_type')
 			dbC = form.cleaned_data.get('db_config')
 			osT = form.cleaned_data.get('os_type')
-			"""
-			TODO:
-				Ejecutar bash script
-				arg1 = configuracion de hardware
-				arg2 = sistema operativo
-				arg3 = bases
-				arg4 = configuracion de bases
-			"""
-			Popen(['bash','tools/bin/cloudlab.sh',str(hwT),str(osT),str(rdbmsL),str(dbC)])
+			Popen(['bash','tools/bin/cloudlab.sh',str(hwT),str(osT),str(dbC),str(db1),str(db2),str(db3)])
 			return redirect('results')
 	else:
 		print("carga")
