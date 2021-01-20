74552af66de9004b604d89e2a0588fab0b5dcf2d
sbrodrig <sbrodrig@espol.edu.ec>
2021-01-19 21:26:42 -0500

    cambio en modelos

--- interfaz/models.py
+++ interfaz/models.py
@@ -16,13 +16,6 @@ class HardwareType(models.Model):
 		return self.name
 
 
-"""class DbConfig(models.Model):
-	name = models.CharField(max_length=10)
-	specifications = models.CharField(max_length=1000)
-	def __str__(self):
-		return self.name
-"""
-
 class OSType(models.Model):
 	name = models.CharField(max_length=20)
 	def __str__(self):
@@ -30,14 +23,13 @@ class OSType(models.Model):
 
 
 class Test(models.Model):
-	SCALE_CHOICES = [(10,'10'), (20,'20'), (30,'30'), ]
+	SCALE_CHOICES = [(5,'5'), (10,'10'), (15,'15'), (20,'20'),]
 
 	rdbms = models.ManyToManyField(Rbdms)
 	hw_type = models.ForeignKey(HardwareType, on_delete=models.CASCADE)
 	hw_spec = models.CharField(max_length=1000, blank=True)
 	os_type = models.ForeignKey(OSType, on_delete=models.CASCADE)
 	scale = models.IntegerField(choices=SCALE_CHOICES, default=10, help_text="TPC-C Workload: Un valor de 10 produce 2GB de data inicial")
-	#confFiles = models.FileField(blank=True, null=True, help_text="Nombrar el archivo con el nombre de la base a aplicarse dicha configuración. MySQL y MariaDB usan la extensión .cnf, PostgreSQL usa extensión .conf. Ejemplo: mysql.cnf - mariadb.cnf - postgresql.conf", validators=[validate_file])
 	db1File = models.FileField(blank=True, null=True, help_text="Extensión .cnf", validators=[validate_file])
 	db2File = models.FileField(blank=True, null=True, help_text="Extensión .conf", validators=[validate_file])
 	db3File = models.FileField(blank=True, null=True, help_text="Extensión .cnf", validators=[validate_file])
--- tools/bin/cloudlab.sh
+++ tools/bin/cloudlab.sh
@@ -45,8 +45,12 @@ then
 		echo "Pronto"
 	elif [[ $2 == "Ubuntu 20.04 LTS" ]]; 
 	then
-		# Por agregar opcion
-		echo "Pronto"
+		/usr/bin/expect <<-EOD
+		spawn $workdir/tools/bin/startExperiment --project ESPOL-sched --duration 2 --name comp-rdbms ESPOL-sched,tpcc-d430-u20
+		expect "Enter PEM pass phrase:"
+		send "$passCred\r"
+		expect eod
+		EOD
 	else
 		echo "The OS does not exists"
 		exit
@@ -67,8 +71,12 @@ then
 		echo "Pronto"
 	elif [[ $2 == "Ubuntu 20.04 LTS" ]];
 	then
-		# Por agregar opcion
-		echo "Pronto"
+		/usr/bin/expect <<-EOD
+		spawn $workdir/tools/bin/startExperiment --project ESPOL-sched --duration 2 --name comp-rdbms ESPOL-sched,tpcc-d710-u20
+		expect "Enter PEM pass phrase:"
+		send "$passCred\r"
+		expect eod
+		EOD
 	else
 		echo "The OS does not exists"
 		exit
